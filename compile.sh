2536f576b0dc49cd695d0ca43c0970603cd352a7
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2022-02-14 22:44:34 +0100

    extract all original Viper4Android VDCs
    Git is not made for large binary files (eg the zip of all vdc or irs files)
    This just makes it so the zip is compressed at compile time
    Also all Original VDCs are now accessible easily

--- compile.sh
+++ compile.sh
@@ -5,6 +5,10 @@ cd ViperIRS
 [ -f "../magiskmodule/common/ViperIRS.zip" ] && rm "../magiskmodule/common/ViperIRS.zip"
 zip -9 "../magiskmodule/common/ViperIRS.zip" *.irs
 cd ..
+cd OriginalVDCs
+[ -f "../magiskmodule/common/vdcs.zip" ] && rm "../magiskmodule/common/vdcs.zip"
+zip -9 "../magiskmodule/common/vdcs.zip" *.vdc
+cd ..
 cd magiskmodule
 zip -r -9 "../$OUTPUT_FILE" .
 cd ..
