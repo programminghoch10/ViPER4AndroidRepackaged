8d5b839c3ead6b0ff743b39a978d04cf0b5ecd5c
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2023-02-26 13:40:09 +0100

    increase wait for boot finished check interval
    this ain't a race, we can wait for a bit

--- magiskmodule/service.sh
+++ magiskmodule/service.sh
@@ -2,7 +2,7 @@
 
 waitUntilBootCompleted() {
   while [ $(getprop sys.boot_completed) -ne 1 ] || [ "$(getprop init.svc.bootanim | tr '[:upper:]' '[:lower:]')" != "stopped" ]; do
-    sleep 1
+    sleep 10
   done
 }
 
